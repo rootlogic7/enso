95e22bb173d0881b22e8630445cfa3277a50e79a
haku <rootlogic7@proton.me>
2026-03-03 07:58:39 +0100

    fix: home-manager in flake.nix

--- flake.nix
+++ flake.nix
@@ -71,7 +71,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = { inherit inputs; }
-	    home-manager.backupFileExtension = "backup";;
+	    home-manager.backupFileExtension = "backup";
             home-manager.users.haku = import ./home/haku.nix;
           }
         ];
