95e598efe881b653ae1ce766959bc61b4eca4c0b
haku <rootlogic7@proton.me>
2026-03-10 20:19:11 +0100

    Create hardware-configuration.nix

---
+++ hosts/quasar/hardware-configuration.nix
@@ -0,0 +1,19 @@
+# hosts/quasar/hardware-configuration.nix
+# Generiert von nixos-generate-config, manuell bereinigt für Disko/Flakes
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
