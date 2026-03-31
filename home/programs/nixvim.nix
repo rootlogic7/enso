82153fcf7cbb3a70a9bd84b0531eef85bccef0f1
haku <haku@horizon.net>
2026-03-31 15:38:57 +0200

    remove neorg and update nixvim

--- home/programs/nixvim.nix
+++ home/programs/nixvim.nix
@@ -7,7 +7,6 @@ in {
     enable = true;
     defaultEditor = true;
 
-    # Dynamisches Theming basierend auf deinen Horizon-Skins
     extraConfigLua = if theme.ui.nixvim_transparent then ''
       vim.cmd [[highlight Normal guifg=#${theme.colors.fg} guibg=NONE ctermbg=NONE]]
       vim.cmd [[highlight NormalNC guifg=#${theme.colors.fg} guibg=NONE ctermbg=NONE]]
@@ -30,15 +29,13 @@ in {
       ignorecase = true;
       smartcase = true;
       updatetime = 50;
-      # WICHTIG für render-markdown und neorg:
-      conceallevel = 2; 
+      conceallevel = 2; # Versteckt die Markdown-Sonderzeichen
     };
 
     globals.mapleader = " ";
 
     plugins = {
-      # 1. BLINK.CMP (Der neue Standard 2026)
-      # Wir nutzen die stabile Version (v1), um Kompatibilitätsprobleme zu vermeiden.
+      # 1. BLINK.CMP
       blink-cmp = {
         enable = true;
         settings = {
@@ -53,49 +50,37 @@ in {
         };
       };
 
-      # 2. RENDER-MARKDOWN (Verschönert MD und Norg im Buffer)
+      # 2. RENDER-MARKDOWN
       render-markdown = {
         enable = true;
         settings = {
-          file_types = [ "markdown" "norg" ];
+          file_types = [ "markdown" ]; # Nur noch Markdown
           latex.enabled = false;
-        };
-      };
-
-      # 3. NEORG (Strukturierte Notizen mit MD-Export)
-      neorg = {
-        enable = true;
-        modules = {
-          "core.defaults".__empty = { };
-          "core.concealer".__empty = { };
-          # Automatischer Export nach Markdown beim Speichern
-          "core.export".__empty = { };
-          "core.export.markdown".__empty = { };
-          "core.dirman".settings = {
-            workspaces = {
-              zentrale = "~/Development/Zentrale";
-              horizon = "~/horizon/docs";
-            };
-            default_workspace = "zentrale";
+          heading = {
+            backgrounds = [ "" "" "" "" "" "" ]; # Keine klobigen Hintergründe mehr
+            foregrounds = [
+              "RenderMarkdownH1" "RenderMarkdownH2" "RenderMarkdownH3"
+              "RenderMarkdownH4" "RenderMarkdownH5" "RenderMarkdownH6"
+            ];
           };
         };
       };
 
-      # 4. LSP & TOOLS
+      # 3. LSP & TOOLS
       lsp = {
         enable = true;
         servers = {
-          nixd.enable = true;      # Beste Nix-Integration
+          nixd.enable = true;
           ansiblels = {
-            enable = true; # Für deine Pi-Konfiguration
+            enable = true;
             package = pkgs.ansible-language-server;
           };
-          marksman.enable = true;  # LSP für Markdown
+          marksman.enable = true; # Der Markdown-Spezialist für Wiki-Links!
           bashls.enable = true;
         };
       };
 
-      # 5. UI & NAVIGATION
+      # 4. UI & NAVIGATION
       lualine.enable = true;
       web-devicons.enable = true;
       which-key.enable = true;
@@ -106,22 +91,30 @@ in {
         settings.highlight.enable = true;
       };
       
-      # Git Integration
       gitsigns.enable = true;
       neogit.enable = true;
     };
 
-    # Automatischer Direnv-Check beim Öffnen von Dateien
     extraConfigLuaPre = ''
-      -- Verhindert das ständige manuelle "direnv allow" innerhalb von Neovim
+      -- Verhindert lästige Direnv-Prompts
       vim.g.direnv_silent_load = 1
+
+      -- Render Markdown Custom Colors (Orientiert am Horizon Skin)
+      vim.cmd [[highlight RenderMarkdownH1 guifg=#${theme.colors.accent_primary} guibg=NONE gui=bold]]
+      vim.cmd [[highlight RenderMarkdownH2 guifg=#${theme.colors.accent_secondary} guibg=NONE gui=bold]]
+      vim.cmd [[highlight RenderMarkdownH3 guifg=#${theme.colors.term.blue} guibg=NONE gui=bold]]
+      vim.cmd [[highlight RenderMarkdownH4 guifg=#${theme.colors.term.magenta} guibg=NONE gui=bold]]
+      vim.cmd [[highlight RenderMarkdownH5 guifg=#${theme.colors.term.green} guibg=NONE gui=bold]]
+      vim.cmd [[highlight RenderMarkdownH6 guifg=#${theme.colors.term.yellow} guibg=NONE gui=bold]]
+      vim.cmd [[highlight RenderMarkdownCode guifg=#${theme.colors.fg} guibg=#${theme.colors.inactive_border}]]
+      vim.cmd [[highlight RenderMarkdownCodeInline guifg=#${theme.colors.term.cyan} guibg=#${theme.colors.inactive_border}]]
+      vim.cmd [[highlight RenderMarkdownBullet guifg=#${theme.colors.accent_primary}]]
+      vim.cmd [[highlight RenderMarkdownQuote guifg=#${theme.colors.accent_tertiary}]]
     '';
 
     keymaps = [
       { action = "<cmd>Neotree toggle<CR>"; key = "<leader>e"; mode = "n"; options.desc = "Explorer"; }
       { action = "<cmd>Neogit<CR>"; key = "<leader>gg"; mode = "n"; options.desc = "Git"; }
-      # Neorg Export Shortcut
-      { action = "<cmd>Neorg export to-file<CR>"; key = "<leader>nx"; mode = "n"; options.desc = "Export Norg to MD"; }
     ];
   };
 }
